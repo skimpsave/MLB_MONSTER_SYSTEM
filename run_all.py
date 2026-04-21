908b43f2258ddd9c691bd727ef7237a8ebb0e097
skimpsave <skimpsave@gmail.com>
2026-04-21 10:44:13 -0400

    Create run_all.py

---
+++ run_all.py
@@ -0,0 +1,7 @@
+import os
+
+os.system("python data_pipeline.py")
+os.system("python model_hr.py")
+os.system("python model_dfs.py")
+os.system("python betting_model.py")
+os.system("python google_sync.py")
